d1092e752d8b3aaae658ccfeb01d9511662f8b78
Etienne Baudry <webmaestro.fr@gmail.com>
2025-02-16 18:11:25 +0000

    Fix static path

--- .netlify/functions/app.ts
+++ .netlify/functions/app.ts
@@ -4,7 +4,7 @@ import serverless from "serverless-http";
 
 import api from "rogemon-api/src";
 
-const staticRootPath = path.resolve(__dirname, "../client/dist");
+const staticRootPath = path.resolve(__dirname, "../../client/dist");
 console.log(`Serving static files from ${staticRootPath}`);
 api.use("/", express.static(staticRootPath));
 
