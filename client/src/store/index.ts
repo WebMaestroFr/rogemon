c8243de2b48bafaea8c7916e7d771afe7eccbe73
Etienne Baudry <webmaestro.fr@gmail.com>
2025-02-17 03:47:12 +0000

    Cleanup, serve static only on dev, add store.debounce function

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -13,4 +13,27 @@ function remove(key: string) {
   localStorage.removeItem(key)
 }
 
-export default { get, set, remove }
+const debounceTimerMap = new Map<string, ReturnType<typeof setTimeout>>()
+export function debounce<T>(key: string, callback: () => Promise<T>, timeout = 2000) {
+  const prevTimer = debounceTimerMap.get(key)
+  if (prevTimer) {
+    clearTimeout(prevTimer)
+  }
+  return new Promise<T>((resolve, reject) => {
+    const nextTimer = setTimeout(
+      () =>
+        callback()
+          .then(resolve)
+          .catch(reject)
+          .finally(() => debounceTimerMap.delete(key)),
+      timeout,
+    )
+    debounceTimerMap.set(key, nextTimer)
+  })
+}
+
+export function isDebouncePending() {
+  return debounceTimerMap.size > 0
+}
+
+export default { debounce, get, set, remove }
