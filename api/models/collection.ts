59a7bac325b0c718fe48f413deb50281bba1f7c0
Maxime <maxime@mjoll.no>
2025-08-04 18:16:32 +0200

    Added Eevee, Ho-oh & Lugia

--- api/models/collection.ts
+++ api/models/collection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export interface ICollection extends Document {
   _id: mongoose.Types.ObjectId
   userId: mongoose.Types.ObjectId
-  expansionId: 'A1' | 'A1a' | 'A2' | 'A2a' | 'A2b' | 'A3' | 'A3a'
+  expansionId: 'A1' | 'A1a' | 'A2' | 'A2a' | 'A2b' | 'A3' | 'A3a' | 'A3b' | 'A4'
   countMap: Map<string, number>
 }
 
@@ -12,7 +12,7 @@ const CollectionSchema = new mongoose.Schema<ICollection>({
   expansionId: {
     type: String,
     required: true,
-    enum: ['A1', 'A1a', 'A2', 'A2a', 'A2b', 'A3', 'A3a'],
+    enum: ['A1', 'A1a', 'A2', 'A2a', 'A2b', 'A3', 'A3a', 'A3b', 'A4'],
   },
   countMap: { type: Map, of: Number, required: true },
 })
--- env.d.ts
+++ env.d.ts
@@ -51,7 +51,7 @@ export interface ICard {
   crafting_cost: number | 'Unknown'
 }
 
-export type ExpansionId = 'A1' | 'A1a' | 'A2' | 'A2a' | 'A2b' | 'A3' | 'A3a'
+export type ExpansionId = 'A1' | 'A1a' | 'A2' | 'A2a' | 'A2b' | 'A3' | 'A3a' | 'A3b' | 'A4'
 
 export interface ICollectionCount {
   [cardId: string]: number
